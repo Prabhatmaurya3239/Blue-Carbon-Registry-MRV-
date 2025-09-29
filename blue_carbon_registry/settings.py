f554fba7a35e8a484ff39b1e1f91e24a78964068
Prabhatmaurya3239 <Prabhatmaurya3239@gmail.com>
2025-09-30 05:04:24 +0530

    Update settings.py

--- blue_carbon_registry/settings.py
+++ blue_carbon_registry/settings.py
@@ -90,9 +90,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # ✅ static folder where logo.png is
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Optional: to serve media files (images uploaded by users)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
